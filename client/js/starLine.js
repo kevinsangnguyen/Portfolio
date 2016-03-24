aa8e26e9f812c4cea6dfc086a0db953b3067a797
Kevin Nguyen <kevinsangnguyen@gmail.com>
2016-03-23 18:40:15 -0700

    hey

--- client/js/starLine.js
+++ client/js/starLine.js
@@ -22,8 +22,8 @@
 
         // create points
         points = [];
-        for(var x = 0; x < width; x = x + width/20) {
-            for(var y = 0; y < height; y = y + height/20) {
+        for(var x = 0; x < width; x = x + width/22) {
+            for(var y = 0; y < height; y = y + height/22) {
                 var px = x + Math.random()*width/20;
                 var py = y + Math.random()*height/20;
                 var p = {x: px, originX: px, y: py, originY: py };
@@ -117,13 +117,13 @@
             ctx.clearRect(0,0,width,height);
             for(var i in points) {
                 // detect points in range
-                if(Math.abs(getDistance(target, points[i])) < 4000) {
+                if(Math.abs(getDistance(target, points[i])) < 1000) {
                     points[i].active = 0.3;
                     points[i].circle.active = 0.6;
-                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                } else if(Math.abs(getDistance(target, points[i])) < 10000) {
                     points[i].active = 0.1;
                     points[i].circle.active = 0.3;
-                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                } else if(Math.abs(getDistance(target, points[i])) < 30000) {
                     points[i].active = 0.02;
                     points[i].circle.active = 0.1;
                 } else {
